7af999ea3126506df3b50b86e3fdacd5ff7f92d1
roman_developer <roman.developer17@gmail.com>
2022-12-26 08:19:07 +0500

    Updated config axios

--- src/http/config.ts
+++ src/http/config.ts
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-const API_PROTOCOL = 'http:';
-const API_HOST = '192.168.0.2';
-const API_PORT = process.env.API_PORT ?? 8000;
-const MAX_REQUEST_TIMEOUT = 30 * 1000;
+const API_PROTOCOL = process.env.API_PROTOCOL || 'http:';
+const API_HOST = process.env.API_HOST || 'localhost';
+const API_PORT = process.env.API_PORT || 8000;
+const API_MAX_REQUEST_TIMEOUT = Number(process.env.API_MAX_REQUEST_TIMEOUT) || 30 * 1000;
 
 const $host = axios.create({
   baseURL: `${API_PROTOCOL}//${API_HOST}:${API_PORT}/api`,
-  timeout: MAX_REQUEST_TIMEOUT,
+  timeout: API_MAX_REQUEST_TIMEOUT,
 });
 
 export default $host;
